29f81f4b093c5d1f0a8bd1a951778135a5cbcc14
Vinícius H <36175157+vinivosh@users.noreply.github.com>
2019-11-10 22:20:10 -0300

    Carregamento de estado no servidor
    O servidor agora carrega o último estado salvo na snapshot e nos logs salvos (arquivos snapshot.pixc e log.pixc, respectivamente, na mesma pasta do servidor).

--- Implementação/ServerTCP.py
+++ Implementação/ServerTCP.py
@@ -12,78 +12,176 @@ global pixelSize
 global screenW
 global screenH
 global timesMod
+global pixels
 
+#Função que salva as modificações num log. Quando o limite do log for ultrapassado, aplica todas modificações documentadas para o snapshot, e prossegue com o logging
+def log(x=0, y=0, color=col.white):
+    global timesMod
+    #Se não atingimos o limite de ações em log (determinado como a quantidade de pixels na malha divido por 32 e arredondado para baixo)...
+    if (timesMod < math.floor(screenH//pixelSize * screenW//pixelSize / 32)-1):
+        if (timesMod == 0):
+            logFile = open("log.pixc", 'w', encoding="utf-8")
+        else:
+            logFile = open("log.pixc", 'a', encoding="utf-8")        
+        logFile.write(str(x) + " " + str(y) + " " + str(color[0]) + " " + str(color[1]) + " " + str(color[2]) + "  ")
+        logFile.close()
+        timesMod += 1
+    else:#Quando a modificação de número máximo de timesMod for ser feita, ela não será logada, mas sim passada pro snapshot
+        dados = {"pixelSize": pixelSize, "screenW": screenW, "screenH": screenH, "pixels": pixels}
+        dataFile = open("snapshot.pixc", 'w', encoding="utf-8")
+        dataFile.write(json.dumps(dados, skipkeys=True))
+        dataFile.close()
+        logFile = open("log.pixc", 'w', encoding="utf-8")
+        logFile.write("")
+        logFile.close()
+        timesMod = 0
+
+#Função que lê um arquivo log e aplica todas as modificações nele na malha de pixels. Se nenhum arquivo for encontrado, ou o arquivo estiver vazio, não faz nada na malha de pixels
+def logRead():
+    try:
+        logFile = open("log.pixc","r")
+        logStr = logFile.read()
+        logFile.close()
+        #print("Log lido em string: " + logStr + "\n")
+        if (logStr == ''):
+            print("Arquivo log encontrado, mas vazio. Prosseguindo sem modificações à malha\n")
+        else:
+            print("Arquivo log encontrado. Modificando a malha...\n")
+            logList = logStr.split()
+            #print("Log lido em lista: " + str(logList) + "\n")
+
+            #Modificando a malha...
+            for i in range (0,len(logList),5):
+                pixels[int(logList[i])][int(logList[i+1])] = (int(logList[i+2]), int(logList[i+3]), int(logList[i+4]))
+            #Salvando os dados na snapshot...
+            dados = {"pixelSize": pixelSize, "screenW": screenW, "screenH": screenH, "pixels": pixels}
+            dataFile = open("snapshot.pixc", 'w', encoding="utf-8")
+            dataFile.write(json.dumps(dados, skipkeys=True))
+            dataFile.close()
+            #Limpando o arquivo de log...
+            logFile = open("log.pixc","w")
+            logFile.write('')
+            logFile.close()
+            print("Malha modificada. Estado carregado com sucesso.\n")
+    except OSError as e:
+        print("Arquivo log não encontrado. Prosseguindo sem modificações à malha\n")
+        pass
+
+useFile = False
 #Tentando ler snapshot salvo
 try:
     dataFile = open("snapshot.pixc","r")
+    print("Arquivo snapshot encontrado. Utilizá-lo? S para sim e N para não (apenas pressione enter para inserir o valor padrão = S")
+    while(True):
+        input_ = input()
+        if (input_ == '' or input_ == 's' or input_ == 'S'):
+            useFile = True
+            break
+        elif (input_ == 'n' or input_ == 'N'):
+            break
+        print("Favor inserir uma entrada válida!")
+    #Armazenando os dados num dict...
+    print("\n")
+    dataDict = json.loads(dataFile.read())
 except OSError as e:
+    print("Arquivo snapshot não encontrado. Prosseguindo com uma malha de pixels nova\n")
     pass
 
-#Selecionando o tamanho do pixel da malha
-print("Insira o tamanho do pixel da malha (Apenas pressione enter para inserir o valor padrão = 32)")
-input_ = ''
-input_ = input()
-while (True):
-    try:
-        if (input_ == ''):
-            pixelSize = 32
-            print("Tamanho do pixel escolhido: 32\n")
-            break
-        pixelSize = int(input_)
-        if (pixelSize > 0):
-            print("Tamanho do pixel escolhido: " + str(pixelSize) + "\n")
-            break
-        else:
-            raise ValueError
+if (useFile == False):
+    #Selecionando o tamanho do pixel da malha
+    print("Insira o tamanho do pixel da malha (Apenas pressione enter para inserir o valor padrão = 32)")
+    input_ = ''
+    input_ = input()
+    while (True):
+        try:
+            if (input_ == ''):
+                pixelSize = 32
+                print("Tamanho do pixel escolhido: 32\n")
+                break
+            pixelSize = int(input_)
+            if (pixelSize > 0):
+                print("Tamanho do pixel escolhido: " + str(pixelSize) + "\n")
+                break
+            else:
+                raise ValueError
                 
-    except:
-        print("Favor inserir um valor numérico válido")
-        input_ = input()
+        except:
+            print("Favor inserir um valor numérico válido")
+            input_ = input()
 
-#Selecionando a largura da tela
-print("Insira a largura da tela de exibição. Será arredondado para o menor múltiplo do tamanho do pixel! (Apenas pressione enter para inserir o valor padrão = 1024)")
-input_ = input()
-while (True):
-    try:
-        if (input_ == ''):
-            screenW = floorToMultiple(1024,pixelSize)
+    #Selecionando a largura da tela
+    print("Insira a largura da tela de exibição. Será arredondado para o menor múltiplo do tamanho do pixel! (Apenas pressione enter para inserir o valor padrão = 1024)")
+    input_ = input()
+    while (True):
+        try:
+            if (input_ == ''):
+                screenW = floorToMultiple(1024,pixelSize)
+                print("Largura da tela escolhida: " + str(screenW) + "\n")
+                break
+            screenW = floorToMultiple(int(input_),pixelSize)
+            if (screenW < pixelSize):
+                screenW = pixelSize
             print("Largura da tela escolhida: " + str(screenW) + "\n")
             break
-        screenW = floorToMultiple(int(input_),pixelSize)
-        if (screenW < pixelSize):
-            screenW = pixelSize
-        print("Largura da tela escolhida: " + str(screenW) + "\n")
-        break
-    except:
-        print("Favor inserir um valor numérico válido")
-        input_ = input()
+        except:
+            print("Favor inserir um valor numérico válido")
+            input_ = input()
 
-#Selecionando a altura da tela
-print("Insira a altura da tela de exibição. Será arredondado para o menor múltiplo do tamanho do pixel! (Apenas pressione enter para inserir o valor padrão = 768)")
-input_ = input()
-while (True):
-    try:
-        if (input_ == ''):
-            screenH = floorToMultiple(768,pixelSize)
+
+    #Selecionando a altura da tela
+    print("Insira a altura da tela de exibição. Será arredondado para o menor múltiplo do tamanho do pixel! (Apenas pressione enter para inserir o valor padrão = 768)")
+    input_ = input()
+    while (True):
+        try:
+            if (input_ == ''):
+                screenH = floorToMultiple(768,pixelSize)
+                print("Altura da tela escolhida: " + str(screenH) + "\n")
+                break
+            screenH = floorToMultiple(int(input_),pixelSize)
+            if (screenH < pixelSize):
+                screenH = pixelSize
             print("Altura da tela escolhida: " + str(screenH) + "\n")
             break
-        screenH = floorToMultiple(int(input_),pixelSize)
-        if (screenH < pixelSize):
-            screenH = pixelSize
-        print("Altura da tela escolhida: " + str(screenH) + "\n")
-        break
-    except:
-        print("Favor inserir um valor numérico válido")
-        input_ = input()
+        except:
+            print("Favor inserir um valor numérico válido")
+            input_ = input()
 
-global pixels
-pixels = [[col.white for i in range(screenH//pixelSize)] for j in range(screenW//pixelSize)]
+    #Salvando informações em arquivo snapshot novo
+    dados = {"pixelSize": pixelSize, "screenW": screenW, "screenH": screenH, "pixels": None}
+    dataFile = open("snapshot.pixc", 'w', encoding="utf-8")
+    dataFile.write(json.dumps(dados, skipkeys=True))
+    dataFile.close()
 
-#Salvando informações em arquivo
-dados = {"pixelSize": pixelSize, "screenW": screenW, "screenH": screenH, "pixels": None}
-dataFile = open("snapshot.pixc", 'w', encoding="utf-8")
-dataFile.write(json.dumps(dados, skipkeys=True))
-dataFile.close()
+    #Cria malha nova
+    pixels = [[col.white for i in range(screenH//pixelSize)] for j in range(screenW//pixelSize)]
+
+else:#Se estivermos utilizando o arquivo snapshot...
+    pixelSize = dataDict["pixelSize"]
+    print("Tamanho do pixel lido: " + str(pixelSize) + "\n")
+    screenW = dataDict["screenW"]
+    print("Largura da tela lido: " + str(screenW) + "\n")
+    screenH = dataDict["screenH"]
+    print("Altura da tela lido: " + str(screenH) + "\n")
+
+    if (dataDict["pixels"] != None):#Se houver uma malha de pixels salva...        
+        pixels = [[col.white for i in range(screenH//pixelSize)] for j in range(screenW//pixelSize)]
+        pixelsTemp = dataDict["pixels"]
+        #print("pixels do arquivo:\n" + str(pixelsTemp) + "\n")
+        #Convertendo a lista interna de volta para tuplas...
+        i = 0  
+        for l1 in pixelsTemp:
+            j = 0
+            for l2 in pixelsTemp[i]:
+                pixels[i][j] = (pixelsTemp[i][j][0], pixelsTemp[i][j][1], pixelsTemp[i][j][2])
+                j += 1
+            i += 1
+        #print("pixels convertidos:\n" + str(pixels) + "\n")
+
+        #Lendo arquivo log e aplicando as modificações
+        logRead()
+
+    else:#Se não houver uma malha de pixels salva...
+        pixels = [[col.white for i in range(screenH//pixelSize)] for j in range(screenW//pixelSize)]
 
 while(True):
     #Selecionando o ip
@@ -142,28 +240,6 @@ print ('[*] Escutando %s:%d' %(ip,port))
 #            for x in lista_clientes:
 #                data = json.dumps({"pixels": pixels,"updateRequest": None, "x": None, "y": None, "color": None, "sair": None})
 #                x.send(data.encode())
-
-#Função que salva as modificações num log. Quando o limite do log for ultrapassado, aplica todas modificações documentadas para o snapshot, e prossegue com o logging
-def log(x=0, y=0, color=col.white):
-    global timesMod
-    #Se não atingimos o limite de ações em log (determinado como a quantidade de pixels na malha divido por 32 e arredondado para baixo)...
-    if (timesMod < math.floor(screenH//pixelSize * screenW//pixelSize / 32)-1):
-        if (timesMod == 0):
-            logFile = open("log.pixc", 'w', encoding="utf-8")
-        else:
-            logFile = open("log.pixc", 'a', encoding="utf-8")        
-        logFile.write(str(x) + " " + str(y) + " " + str(color[0]) + " " + str(color[1]) + " " + str(color[2]) + "  ")
-        logFile.close()
-        timesMod += 1
-    else:#Quando a modificação de número máximo de timesMod for ser feita, ela não será logada, mas sim passada pro snapshot
-        dados = {"pixelSize": pixelSize, "screenW": screenW, "screenH": screenH, "pixels": pixels}
-        dataFile = open("snapshot.pixc", 'w', encoding="utf-8")
-        dataFile.write(json.dumps(dados, skipkeys=True))
-        dataFile.close()
-        logFile = open("log.pixc", 'w', encoding="utf-8")
-        logFile.write("")
-        logFile.close()
-        timesMod = 0
 
 def handle_client(client_socket):
     print ('\n-------------------\n')
